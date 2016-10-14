80e44df004cd7fb33f38eb2be21f1081f8576e0a
Rita Zerrizuela <zeta@widcket.com>
2016-10-14 12:55:14 -0300

    Added start sequence

--- bin/iris.js
+++ bin/iris.js
@@ -14,6 +14,7 @@ const Vantage = require('vantage');
 const Threads = require('threads');
 const dispatcher = require('../lib/Dispatcher');
 const minimist = require('minimist');
+const ora = require('ora');
 const chalk = require('chalk');
 
 const args = minimist(process.argv.slice(2));
@@ -30,8 +31,6 @@ const banner =
     `
           _          _            _         _        \r\n         \/\\ \\       \/\\ \\         \/\\ \\      \/ \/\\      \r\n         \\ \\ \\     \/  \\ \\        \\ \\ \\    \/ \/  \\     \r\n         \/\\ \\_\\   \/ \/\\ \\ \\       \/\\ \\_\\  \/ \/ \/\\ \\__  \r\n        \/ \/\\\/_\/  \/ \/ \/\\ \\_\\     \/ \/\\\/_\/ \/ \/ \/\\ \\___\\ \r\n       \/ \/ \/    \/ \/ \/_\/ \/ \/    \/ \/ \/    \\ \\ \\ \\\/___\/ \r\n      \/ \/ \/    \/ \/ \/__\\\/ \/    \/ \/ \/      \\ \\ \\       \r\n     \/ \/ \/    \/ \/ \/_____\/    \/ \/ \/   _    \\ \\ \\      \r\n ___\/ \/ \/__  \/ \/ \/\\ \\ \\  ___\/ \/ \/__ \/_\/\\__\/ \/ \/      \r\n\/\\__\\\/_\/___\\\/ \/ \/  \\ \\ \\\/\\__\\\/_\/___\\\\ \\\/___\/ \/       \r\n\\\/_________\/\\\/_\/    \\_\\\/\\\/_________\/ \\_____\\\/\n
     `;
-
-var iris, config, threadPool;
 
 const vantage = (config) => {
     if (config.vantage.enabled) {
@@ -52,29 +51,41 @@ const cli = () => {
 }
 
 const init = (env) => {
-    if (env.configPath) {
-        process.chdir(env.configBase);
-    } else if (!env.modulePath) {
+    var iris, config, threadPool;
+    var spinner = ora('Loading local package');
+
+    console.log(banner);
+    spinner.start();
+
+    if (!env.modulePath) {
         // TODO: Prettify logs and messages
+        spinner.fail();
         console.log('Local Iris not found.');
         console.log('Try running: npm install iris --save');
 
         process.exit(1);
+    }
+
+    spinner.succeed();
+    spinner = ora('Loading Irisfile').start();
+
+    if (env.configPath) {
+        process.chdir(env.configBase);
+        spinner.succeed();
     } else {
         // TODO: Prettify logs and messages
+        spinner.fail();
         console.log('No Irisfile found.');
 
         process.exit(1);
     }
 
     require(env.configPath);
-
     iris = require(env.modulePath);
+
     threadPool = new Threads.Pool(iris.config.threads);
 
     if (iris.flows.length > 0) {
-        console.log(banner);
-
         dispatcher.threadPool = threadPool;
         dispatcher.config = {
             flows: iris.flows
