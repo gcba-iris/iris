dd6f196fde107a3d39ee1ebf2ea0b7f0a7309f61
Rita Zerrizuela <zeta@widcket.com>
2016-10-18 17:04:44 -0300

    Added dock tests

--- tests/core/dock.js
+++ tests/core/dock.js
@@ -1,170 +1,356 @@
 const Dock = require('../../lib/bases/Dock');
+const Sparkles = require('sparkles');
 const test = require('tape');
-const mockery = require('mockery');
 const group = require('tape-plus').group;
 
-const dock = new Dock('test', 'test');
-
-const setMockery = (test) => {
-    test.beforeEach((t) => {
-        mockery.enable();
-    });
-
-    test.afterEach((t) => {
-        mockery.disable();
-    });
+const config = {
+    port: 8080,
+    parser: {
+        subtagSeparator: '|',
+        dataSeparator: ','
+    },
+    encoder: {
+        subtagSeparator: '|',
+        dataSeparator: ','
+    },
+    maxMessageLength: 100
+};
+const dispatcher = {
+    tags: {
+        'tag1': {}
+    },
+    dispatch: (data, callback) => {}
 };
 
 group('id', (test) => {
-    test('sets dock Id', (t) => {
+    var dock = new Dock('test', 'test');
 
-        t.pass('Ok');
+    test('sets dock Id', (t) => {
+        dock.id = 'test';
+        t.equal(dock.id, 'test');
     });
 
     test('gets dock Id', (t) => {
-
-        t.pass('Ok');
+        dock.id = 'example';
+        t.equal(dock.id, 'example');
     });
 });
 
 group('name', (test) => {
-    test('gets dock name', (t) => {
+    const dock = new Dock('test', 'test');
 
-        t.pass('Ok');
+    test('gets dock name', (t) => {
+        t.equal(dock.name, 'test');
     });
 });
 
 group('protocol', (test) => {
-    test('gets dock protocol', (t) => {
+    const dock = new Dock('test', 'test');
 
-        t.pass('Ok');
+    test('gets dock protocol', (t) => {
+        t.equal(dock.protocol, 'test');
     });
 });
 
 group('port', (test) => {
-    test('gets dock port', (t) => {
+    const dock = new Dock('test', 'test');
 
-        t.pass('Ok');
+    test('gets dock port', (t) => {
+        dock.config = {
+            port: 8080
+        };
+        t.equal(dock.port, 8080);
     });
 });
 
 group('config', (test) => {
-    test('sets dock config', (t) => {
+    const dock = new Dock('test', 'test');
 
-        t.pass('Ok');
+    const config = {
+        port: 8080,
+        test: 'test'
+    };
+    const expectedConfig = {
+        maxMessageLength: 300,
+        port: 8080,
+        test: 'test'
+    };
+
+    test('sets dock config', (t) => {
+        dock.config = config;
+        t.deepEqual(dock.config, expectedConfig);
     });
 
     test('gets dock config', (t) => {
-
-        t.pass('Ok');
+        t.deepEqual(dock.config, expectedConfig);
     });
 });
 
 group('dispatcher', (test) => {
-    test('sets dispatcher instance', (t) => {
+    const dock = new Dock('test', 'test');
 
-        t.pass('Ok');
+    test('sets dispatcher instance', (t) => {
+        dock.dispatcher = dispatcher;
+        t.deepEqual(dock._dispatcher, dispatcher);
     });
 });
 
 group('validate()', (test) => {
-    setMockery(test);
+    const dock = new Dock('test', 'test');
+
+    const config = {
+        port: 8080,
+        parser: {
+            subtagSeparator: '|',
+            dataSeparator: ','
+        },
+        encoder: {
+            subtagSeparator: '|',
+            dataSeparator: ','
+        },
+        maxMessageLength: 100
+    };
+
+    dock.dispatcher = dispatcher;
+    dock.config = config;
 
     test('validates messages', (t) => {
+        const result = dock.validate('tag1|subtag1|02,56,58,8|subtag2|sds,sd,wtr,ghd');
+        const expectedResult = {
+            tag: 'tag1',
+            message: ['subtag1', '02,56,58,8', 'subtag2', 'sds,sd,wtr,ghd']
+        };
 
-        t.pass('Ok');
+        t.deepEqual(result, expectedResult);
     });
 
     test('checks message length', (t) => {
+        const result = dock.validate('tag1|subtag1|02,56,58,8,69,45,78,65,125,69,898,98|subtag2|sds,sd,wtr,ghd,sd,rt,frt,uty,adf,wfg,dgs,sd');
 
-        t.pass('Ok');
+        t.equal(result, false);
     });
 
     test('checks message tag', (t) => {
+        const result = dock.validate('tag2|subtag1|02,56,58,8|subtag2|sds,sd,wtr,ghd');
 
-        t.pass('Ok');
+        t.equal(result, false);
     });
 
     test('checks message format', (t) => {
+        const result = dock.validate('tag1|subtag1|02;56;58;8|subtag2|sds;sd;wtr;ghd');
 
-        t.pass('Ok');
+        t.equal(result, false);
     });
 });
 
 group('parse()', (test) => {
-    setMockery(test);
+    const dock = new Dock('test', 'test');
+
+    dock.dispatcher = dispatcher;
+    dock.config = config;
 
     test('parses messages', (t) => {
+        const data = {
+            tag: 'tag1',
+            message: ['subtag1', '02,56,58,8', 'subtag2', 'sds,sd,wtr,ghd']
+        };
+        const meta = {
+            test: 'test'
+        };
+        const result = dock.parse(data, meta);
+        const expectedResult = {
+            tag: 'tag1',
+            meta: meta,
+            data: {
+                'subtag1': ['02', '56', '58', '8'],
+                'subtag2': ['sds', 'sd', 'wtr', 'ghd']
+            }
+        };
 
-        t.pass('Ok');
+        t.deepEqual(result, expectedResult);
+    });
+
+    test('checks message body', (t) => {
+        const data = {
+            tag: 'tag1',
+            message: '|subtag1|02,56,58,8|subtag2|sds,sd,wtr,ghd'
+        };
+        const meta = {};
+        const result = dock.parse(data, meta);
+
+        t.equal(result, null);
     });
 });
 
 group('process()', (test) => {
-    setMockery(test);
+    const dock = new Dock('test', 'test');
+    const events = Sparkles('test');
 
-    test('sends messages to dispatcher', (t) => {
+    const message = 'tag1|subtag1|02,56,58,8|subtag2|sds,sd,wtr,ghd';
+    const meta = {};
+    const callback = () => {};
 
-        t.pass('Ok');
+    dock.id = 'test';
+    dock.config = config;
+
+    test('sends messages to dispatcher', (t, next) => {
+        var failed = true;
+        const eventCallback = (data) => {
+            failed = false;
+
+            t.pass('Ok');
+            next();
+        };
+        const dispatcher = {
+            tags: {
+                'tag1': {}
+            },
+            dispatch: (data, callback) => {
+                events.emit('dispatcherExecuted', {});
+            }
+        };
+
+        events.on('dispatcherExecuted', eventCallback.bind(this));
+        dock.dispatcher = dispatcher;
+        dock.process(message, meta, callback);
+
+        setTimeout(() => {
+            if (failed) {
+                t.fail('dispatcher.dispatch() was never called');
+                next();
+            }
+        }, 5);
     });
 
-    test('sets dock Id', (t) => {
-        //t.equal(add(base, -1), 9)
-        t.pass('Ok');
+    test('sets dock Id', (t, next) => {
+        var failed = true;
+        const eventCallback = (data) => {
+            failed = false;
+
+            t.pass('Ok');
+            next();
+        };
+        const dispatcher = {
+            tags: {
+                'tag1': {}
+            },
+            dispatch: (data, callback) => {
+                if (data.meta.dock) events.emit('setDockId', {});
+            }
+        };
+
+        events.on('setDockId', eventCallback.bind(this));
+        dock.dispatcher = dispatcher;
+        dock.process(message, meta, callback);
+
+        setTimeout(() => {
+            if (failed) {
+                t.fail('Dock id was never set');
+                next();
+            }
+        }, 5);
     });
 
-    test('sets timestamp', (t) => {
+    test('sets message timestamp', (t, next) => {
+        var failed = true;
+        const eventCallback = (data) => {
+            failed = false;
 
-        t.pass('Ok');
-    });
+            t.pass('Ok');
+            next();
+        };
+        const dispatcher = {
+            tags: {
+                'tag1': {}
+            },
+            dispatch: (data, callback) => {
+                if (data.meta.timestamp) events.emit('setTimestamp', {});
+            }
+        };
 
-    test('emits \'message\' event', (t) => {
+        events.on('setTimestamp', eventCallback.bind(this));
+        dock.dispatcher = dispatcher;
+        dock.process(message, meta, callback);
 
-        t.pass('Ok');
-    });
-});
-
-group('reply()', (test) => {
-    setMockery(test);
-
-    test('emits \'response\' event', (t) => {
-
-        t.pass('Ok');
+        setTimeout(() => {
+            if (failed) {
+                t.fail('Timestamp was never set');
+                next();
+            }
+        }, 5);
     });
 });
 
 group('encode()', (test) => {
-    setMockery(test);
+    var dock = new Dock('test', 'test');
 
-    test('encodes messages', (t) => {
+    dock.id = 'test';
+    dock.config = config;
+    dock.dispatcher = dispatcher;
 
-        t.pass('Ok');
+    test('encodes array messages', (t) => {
+        const result = dock.encode({
+            message: ['abc', 'def', 'ghi']
+        });
+
+        t.equal(result, 'abc,def,ghi');
     });
-});
 
-group('_emitEvent()', (test) => {
-    setMockery(test);
+    test('encodes object messages', (t) => {
+        const result = dock.encode({
+            message: {
+                subtag1: ['abc', 'def', 'ghi'],
+                subtag2: ['jkl', 'mno', 'pqr']
+            }
+        });
 
-    test('emits events', (t) => {
-
-        t.pass('Ok');
+        t.equal(result, '|subtag1|abc,def,ghi|subtag2|jkl,mno,pqr');
     });
-});
 
-group('_invalidMessage()', (test) => {
-    setMockery(test);
+    test('encodes messages with toString() method', (t) => {
+        const message = {};
+        message.prototype = {
+            toString: () => 'Test'
+        };
 
-    test('emits emits \'invalidMessage\' event', (t) => {
+        const result = dock.encode({
+            message: message
+        });
 
-        t.pass('Ok');
+        t.equal(result, 'Test');
+    });
+
+    test('returns string messages', (t) => {
+        const result = dock.encode({
+            message: 'Test'
+        });
+
+        t.equal(result, 'Test');
+    });
+
+    test('handles unknown message types', (t) => {
+        const result = dock.encode({
+            message: new Date()
+        });
+
+        t.equal(result, '');
     });
 });
 
 group('_checkConfig()', (test) => {
-    setMockery(test);
+    var dock = new Dock('test', 'test');
+
+    var failed = false;
+
+    dock.id = 'test';
+    dock.config = config;
+    dock.dispatcher = dispatcher;
+    dock.logger.error = () => {
+        failed = true;
+    };
 
     test('checks config', (t) => {
-
-        t.pass('Ok');
+        if (failed) t.fail('Config threw validation errors');
+        else t.pass('Ok');
     });
 });
