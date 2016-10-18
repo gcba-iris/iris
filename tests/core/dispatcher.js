4210c1c47f6ea6d37c669d4a2c9920c5e6e55e6f
Rita Zerrizuela <zeta@widcket.com>
2016-10-18 19:07:07 -0300

    Added dispatcher tests stubs

--- tests/core/dispatcher.js
+++ tests/core/dispatcher.js
@@ -1,6 +1,97 @@
 'use strict';
 
 const Sparkles = require('sparkles');
-const dispatcher = require('../../lib/dispatcher');
+const Flow = require('../../lib/Flow');
+const dispatcher = require('../../lib/Dispatcher');
 const test = require('tape');
 const group = require('tape-plus').group;
+const httpDock = require('../../example/docks/http');
+const handler = require('../../example/handlers/handler1');
+const hook1 = require('../../example/hooks/hook1');
+const hook2 = require('../../example/hooks/hook2');
+
+const flows = [new Flow('Flow 1', {
+        tag: 'tag1',
+        docks: [httpDock],
+        handler: handler,
+        inputHooks: [hook1],
+        outputHooks: [hook2]
+    }),
+    new Flow('Flow 2', {
+        tag: 'tag2',
+        docks: [httpDock],
+        handler: handler,
+        inputHooks: [hook1],
+        outputHooks: [hook2]
+    })
+];
+
+group('tags', (test) => {
+    test('gets tags', (t) => {
+        t.pass('Ok');
+    });
+});
+
+group('config', (test) => {
+    test('sets dispatcher config', (t) => {
+        t.pass('Ok');
+    });
+});
+
+group('threadPool', (test) => {
+    test('sets dispatcher threadpool instance', (t) => {
+        t.pass('Ok');
+    });
+});
+
+group('dispatch()', (test) => {
+    test('sends data to threadpool', (t) => {
+        t.pass('Ok');
+    });
+
+    test('adds flow name to metadata', (t) => {
+        t.pass('Ok');
+    });
+
+    test('receives response from threadpool', (t) => {
+        t.pass('Ok');
+    });
+});
+
+group('respond()', (test) => {
+    test('sends response to original dock', (t) => {
+        t.pass('Ok');
+    });
+});
+
+group('_processFlows()', (test) => {
+    test('pushes content to tags array', (t) => {
+        t.pass('Ok');
+    });
+});
+
+group('_generateJob()', (test) => {
+    test('generates a valid javascript file', (t) => {
+        t.pass('Ok');
+    });
+});
+
+group('_loadJob()', (test) => {
+    test('loads compiled flows into threadpool', (t) => {
+        t.pass('Ok');
+    });
+
+    test('starts docks', (t) => {
+        t.pass('Ok');
+    });
+});
+
+group('_startDock()', (test) => {
+    test('stops docks', (t) => {
+        t.pass('Ok');
+    });
+
+    test('starts docks', (t) => {
+        t.pass('Ok');
+    });
+});
