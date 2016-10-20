aa321f1133a10bda19c7dcaa8191892102cc6388
Rita Zerrizuela <zeta@widcket.com>
2016-10-20 17:50:36 -0300

    Updated tests

--- tests/core/utils.js
+++ tests/core/utils.js
@@ -7,12 +7,14 @@ const util = require('util');
 
 group('utils.log.error()', (test) => {
     test('logs errors', (t) => {
+        log.log.error('Test');
         t.equal(util.isFunction(log.log.error), true);
     });
 });
 
 group('utils.log.warn()', (test) => {
     test('logs warnings', (t) => {
+        log.log.warn('Test');
         t.equal(util.isFunction(log.log.warn), true);
     });
 });
