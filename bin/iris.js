9b9e4419c4d0e302b34781935211c364c4c2cc8f
Rita Zerrizuela <zeta@widcket.com>
2016-11-01 10:06:53 -0300

    Version bump

--- bin/iris.js
+++ bin/iris.js
@@ -33,9 +33,9 @@ const loader = new LiftOff({
         '.js': null
     },
     v8flags: [
-        '--stack_size=8192',
-        '--max-old-space-size=8192',
-        '–max-new-space-size=2048'
+        process.env.STACK_SIZE || '--stack_size=8192',
+        process.env.MAX_OLD_SPACE_SIZE || '--max-old-space-size=8192',
+        process.env.MAX_NEW_SPACE_SIZE || '–max-new-space-size=2048'
     ]
 });
 
